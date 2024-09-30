fd62b0aa4412de6d4f3ee4f9456a0677260fa260
Niharika Amritkar <138382151+NiharikaAmritkar@users.noreply.github.com>
2024-09-30 11:19:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,22 +12,9 @@ import os
 # Show title and description.
 st.title("📄 Document question answering")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Upload an image below and ask a question about it! "
 )
-# Load the models and processor once when the app starts
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-# openai_api_key = st.text_input("OpenAI API Key", type="password")
-# if not openai_api_key:
-#     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-# else:
 
-#     # Create an OpenAI client.
-#     client = OpenAI(api_key=openai_api_key)
-
-    # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
     "Upload an image (.png or .jpg or .jpeg)", type=("png", "jpg", "jpeg")
 )
@@ -106,24 +93,3 @@ if uploaded_file is not None:
     st.write(output_text)
    
 
-
-# if uploaded_file and text_query:
-
-#     # Process the uploaded file and question.
-#     document = uploaded_file.read().decode()
-#     messages = [
-#         {
-#             "role": "user",
-#             "content": f"Here's a document: {document} \n\n---\n\n {text_query}",
-#         }
-#     ]
-
-    # Generate an answer using the OpenAI API.
-    # stream = client.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=messages,
-    #     stream=True,
-    # )
-
-    # Stream the response to the app using `st.write_stream`.
-    # st.write_stream(stream)
